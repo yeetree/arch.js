0476d4d713bcc3cd0a602aca5261ebf384096396
yeetree <64941543+yeetree@users.noreply.github.com>
2024-02-02 15:42:36 -0600

    yes

--- arch/arch.js
+++ arch/arch.js
@@ -1,5 +1,5 @@
 class archjs {
-    ram = new memory(1048576);
-    storage = new memory(65536);
+    ram = new memory(65536);
+    storage = new memory(1048576);
     registers = new registers();
 }
